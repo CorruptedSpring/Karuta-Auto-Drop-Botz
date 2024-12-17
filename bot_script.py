5e27364ec7aaa6b9cdc354bb4c600fac890620fa
Corrupted <youssefelajbari@gmail.com>
2024-12-17 09:56:46 +0100

    Update bot_script.py

--- bot_script.py
+++ bot_script.py
@@ -7,13 +7,14 @@ def GetData():
         return data
 
 def DropKaruta(ChannelID, Token):
-    url = f"https://discord.com/api/v9/channels/1284893215467507773/messages"
+    url = f"https://discord.com/api/v9/channels/{ChannelID}/messages"
+    
     headers = {
          Authorization: Token
          
     }
     {"mobile_network_type":"unknown","content":"ok","nonce":"1318296299350523904","tts":false,"flags":0}
-    requests = requests.post(url, headers=headers)
+    req = requests.post(url)
          
 
 ChannelID = GetData()[0].strip()
